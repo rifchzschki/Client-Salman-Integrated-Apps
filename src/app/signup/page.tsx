c2b9dd8ec608ed460a11324b44e13b22bd947339
rifchzschki <114592995+rifchzschki@users.noreply.github.com>
2025-05-26 22:04:12 +0700

    fix: register bug for deploy

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -84,7 +84,7 @@ export default function Register() {
 
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/register`,
+        `${process.env.NEXT_PUBLIC_API_URL}/register`,
         {
           method: "POST",
           headers: {
